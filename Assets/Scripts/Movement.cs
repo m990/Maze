bd34d2f02ec09c56d24a42814e3efd55e653735a
m990 <m990@users.noreply.github.com>
2017-10-04 12:24:48 -0700

    Maze Game
    This is something I'm doing for school that I don't want to loose, so I'm making a backup of

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	static float z = 50f;
+	static float y = 0f;
+	static float x = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
